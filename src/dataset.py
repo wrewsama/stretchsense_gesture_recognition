bf7633a2f914b1146b786a5524d606ade7bed2b7
wrewsama <andrewlzs512@gmail.com>
2022-06-03 17:21:09 +0800

    Create dataset class

--- src/dataset.py
+++ src/dataset.py
@@ -7,10 +7,15 @@ class CapacitanceDataset(Dataset):
     """Encapsulates a data set of capacitance values and labels."""
 
     def __init__(self, filename: str):
-        pass
+        data_file = pd.read_csv(filename)
+        labels = data_file.iloc[:, 0].values
+        inputs = data_file.iloc[ :, 2:9].values
+
+        self.inputs = torch.tensor(inputs, dtype=torch.float32)
+        self.labels = torch.tensor(labels)
 
     def __len__(self) -> int:
-        pass
+        return len(self.labels)
 
     def __getitem__(self, idx: int) -> Tuple[torch.tensor, torch.tensor]:
-        pass
+        return self.inputs[idx], self.labels[idx]
