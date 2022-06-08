b155ce8177e7f7a0a736a35f8e2db63bdde673d0
wrewsama <andrewlzs512@gmail.com>
2022-06-08 17:21:28 +0800

    Write script to get result from given sensor input

--- app.py
+++ app.py
@@ -4,7 +4,17 @@ from src.models import feed_forward
 def main():
     model = feed_forward.FeedForwardModel(7, 3, 32)
     model.load_state_dict(torch.load("trained_models/example_model.pth"))
-    print(model(torch.tensor([125.9,279.0,131.4,304.0,297.3,282.8,286.9])))
+
+    gestures = ["rock",
+                "paper",
+                "scissors",]
+
+    testdata = [125.9,279.0,131.4,304.0,297.3,282.8,286.9]
+    output = model(torch.tensor(testdata))
+    resultidx = torch.argmax(output).item()
+    result = gestures[resultidx]
+    
+    print(result)
 
 if __name__ == "__main__":
     main()
